b2f44a6f5fd9b33308cb60c1b1385f8ec2beb324
be4n01 <107491716+be4n01@users.noreply.github.com>
2022-09-21 19:24:01 +0300

    Aesthetic Fixes

--- proj1/Form1.Designer.cs
+++ proj1/Form1.Designer.cs
@@ -177,6 +177,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Red;
             this.label3.Location = new System.Drawing.Point(1130, 9);

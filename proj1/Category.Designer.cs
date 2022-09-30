b4bd5c05b3f4d83a28f56adacc6efad7ae34d1c8
SammyG02 <samuelgosaye02@gmail.com>
2022-09-30 11:02:43 +0300

    category layout fixed

--- proj1/Category.Designer.cs
+++ proj1/Category.Designer.cs
@@ -48,18 +48,20 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Location = new System.Drawing.Point(13, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 30);
+            this.label1.Size = new System.Drawing.Size(43, 37);
             this.label1.TabIndex = 0;
             this.label1.Text = "Id";
             // 
             // txtId
             // 
             this.txtId.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.txtId.Location = new System.Drawing.Point(63, 40);
+            this.txtId.Location = new System.Drawing.Point(20, 103);
+            this.txtId.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtId.Name = "txtId";
-            this.txtId.Size = new System.Drawing.Size(183, 20);
+            this.txtId.Size = new System.Drawing.Size(243, 22);
             this.txtId.TabIndex = 1;
             this.txtId.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -68,29 +70,31 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.label2.Location = new System.Drawing.Point(12, 80);
+            this.label2.Location = new System.Drawing.Point(298, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(166, 30);
+            this.label2.Size = new System.Drawing.Size(219, 37);
             this.label2.TabIndex = 2;
             this.label2.Text = "Category Name";
             // 
             // categoryname
             // 
             this.categoryname.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.categoryname.Location = new System.Drawing.Point(194, 90);
+            this.categoryname.Location = new System.Drawing.Point(305, 103);
+            this.categoryname.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.categoryname.Name = "categoryname";
-            this.categoryname.Size = new System.Drawing.Size(229, 20);
+            this.categoryname.Size = new System.Drawing.Size(304, 22);
             this.categoryname.TabIndex = 3;
             // 
             // DGV
             // 
             this.DGV.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
             this.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGV.Location = new System.Drawing.Point(7, 165);
-            this.DGV.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.DGV.Location = new System.Drawing.Point(9, 203);
+            this.DGV.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.DGV.Name = "DGV";
             this.DGV.RowHeadersWidth = 51;
-            this.DGV.Size = new System.Drawing.Size(526, 165);
+            this.DGV.Size = new System.Drawing.Size(868, 258);
             this.DGV.TabIndex = 4;
             this.DGV.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGV_CellClick);
             this.DGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGV_CellContentClick);
@@ -101,9 +105,10 @@
             this.Addbtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Addbtn.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Addbtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.Addbtn.Location = new System.Drawing.Point(7, 341);
+            this.Addbtn.Location = new System.Drawing.Point(13, 504);
+            this.Addbtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Addbtn.Name = "Addbtn";
-            this.Addbtn.Size = new System.Drawing.Size(121, 41);
+            this.Addbtn.Size = new System.Drawing.Size(200, 72);
             this.Addbtn.TabIndex = 5;
             this.Addbtn.Text = "Add";
             this.Addbtn.UseVisualStyleBackColor = false;
@@ -115,9 +120,10 @@
             this.updatebtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.updatebtn.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.updatebtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.updatebtn.Location = new System.Drawing.Point(141, 341);
+            this.updatebtn.Location = new System.Drawing.Point(233, 504);
+            this.updatebtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.updatebtn.Name = "updatebtn";
-            this.updatebtn.Size = new System.Drawing.Size(121, 41);
+            this.updatebtn.Size = new System.Drawing.Size(200, 72);
             this.updatebtn.TabIndex = 6;
             this.updatebtn.Text = "Update";
             this.updatebtn.UseVisualStyleBackColor = false;
@@ -129,9 +135,10 @@
             this.Deletbtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Deletbtn.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold);
             this.Deletbtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.Deletbtn.Location = new System.Drawing.Point(277, 341);
+            this.Deletbtn.Location = new System.Drawing.Point(453, 504);
+            this.Deletbtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Deletbtn.Name = "Deletbtn";
-            this.Deletbtn.Size = new System.Drawing.Size(121, 41);
+            this.Deletbtn.Size = new System.Drawing.Size(200, 72);
             this.Deletbtn.TabIndex = 7;
             this.Deletbtn.Text = "Delete ";
             this.Deletbtn.UseVisualStyleBackColor = false;
@@ -143,9 +150,10 @@
             this.Clearbtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Clearbtn.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Clearbtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.Clearbtn.Location = new System.Drawing.Point(412, 341);
+            this.Clearbtn.Location = new System.Drawing.Point(677, 504);
+            this.Clearbtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Clearbtn.Name = "Clearbtn";
-            this.Clearbtn.Size = new System.Drawing.Size(121, 41);
+            this.Clearbtn.Size = new System.Drawing.Size(200, 72);
             this.Clearbtn.TabIndex = 8;
             this.Clearbtn.Text = "Clear";
             this.Clearbtn.UseVisualStyleBackColor = false;
@@ -157,10 +165,10 @@
             // 
             // Category
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
-            this.ClientSize = new System.Drawing.Size(675, 520);
+            this.ClientSize = new System.Drawing.Size(890, 644);
             this.Controls.Add(this.Clearbtn);
             this.Controls.Add(this.Deletbtn);
             this.Controls.Add(this.updatebtn);
@@ -171,6 +179,7 @@
             this.Controls.Add(this.txtId);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Category";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Category";

df822d9734603330b2fe992e0d37ae5947ace781
be4n01 <107491716+be4n01@users.noreply.github.com>
2022-09-30 17:27:44 +0300

    Changed Contact Us to Contact

--- proj1/Program.cs
+++ proj1/Program.cs
@@ -16,7 +16,7 @@ namespace proj1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SellingPlace());
+            Application.Run(new Form1());
         }
     }
 }
